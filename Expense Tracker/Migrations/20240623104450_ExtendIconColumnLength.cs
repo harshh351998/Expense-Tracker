dddc1167204aedf071a6b9c3b1af4e85587de059
harshh351998 <mendapara.harsh47@gmail.com>
2024-07-04 10:34:06 +0530

    Add project files.

---
+++ Expense Tracker/Migrations/20240623104450_ExtendIconColumnLength.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker.Migrations
+{
+    public partial class ExtendIconColumnLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Icon",
+                table: "Categories",
+                type: "nvarchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Icon",
+                table: "Categories",
+                type: "nvarchar(10)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+        }
+    }
+}
